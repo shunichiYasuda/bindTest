86bc80bd318cdaecf896fa3ecf2b8c0e2190d1f1
shuni <shuni@192.168.3.13>
2022-04-09 07:03:26 +0900

    edited openAction(),savaAction()

--- src/main/java/com/GenSci/sample/bindtest/PrimaryController.java
+++ src/main/java/com/GenSci/sample/bindtest/PrimaryController.java
@@ -1,11 +1,15 @@
 package com.GenSci.sample.bindtest;
 
+import java.io.File;
+
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 
 public class PrimaryController {
 
@@ -19,9 +23,47 @@ public class PrimaryController {
 	Canvas aveCanvas;
 	@FXML
 	Canvas typeCanvas;
+	@FXML
+	Button openBtn;
+	@FXML
+	Button saveBtn;
+	@FXML
+	Button quitBtn;
+	@FXML
+	private void quitAction() {
+		System.exit(0);
+	}
 	//
 	GraphicsContext g;
-	
+	//
+	String dir = null;
+	@FXML
+	private void openAction() {
+		File file;
+		FileChooser fc = new FileChooser();
+		fc.setTitle("open File");
+		if(dir != null) {
+			fc.setInitialDirectory(new File(dir));
+			file = fc.showOpenDialog(null);
+		}else {
+			file = fc.showOpenDialog(null);
+			dir = file.getParent();
+		}
+		System.out.println(dir);
+	}
+	@FXML
+	private void saveAction() {
+		File file;
+		FileChooser fc = new FileChooser();
+		fc.setTitle("save File");
+		if(dir != null) {
+			fc.setInitialDirectory(new File(dir));
+			file = fc.showSaveDialog(null);
+		}else {
+			file = fc.showSaveDialog(null);
+			dir = file.getParent();
+		}
+	}
 	
 	
 	@FXML
